8b8569898f98048ac698823db85f3616114fac72
John Trecker <jttrecker@gmail.com>
2019-12-01 00:18:31 -0800

    refactor js

--- main.js
+++ main.js
@@ -12,115 +12,81 @@ const insults = {
   ]
 }
 
-class Insult{
+class Insult {
   constructor() {
     this.adjectives = insults['adjective'];
     this.firstNoun = insults['noun_one'];
     this.secondNoun = insults['noun_two'];
+
+    this.panelWidth = 120;
+    this.SLOTS_PER_REEL = 16
+    this.REEL_RADIUS = Math.round((this.panelWidth / 2) / Math.tan(Math.PI / this.SLOTS_PER_REEL));
   }
 
-  generateInsult(){
-    const i = this.random(0, this.adjectives.length - 1)
-    const j = this.random(0, this.firstNoun.length - 1)
-    const k = this.random(0, this.secondNoun.length - 1)
+  createSlots(ring, options) {
+    let slotAngle = 360 / this.SLOTS_PER_REEL;
+    let randomInt = this.getRandom();
 
-    const adj = this.adjectives[i]
-    const noun = this.firstNoun[j]
-    const anotherNoun = this.secondNoun[k]
+    for (var i = 0; i < this.SLOTS_PER_REEL; i++) {
+      let slot = document.createElement('div');
 
-    return `You\'re just a ${adj} ${noun} ${anotherNoun}`
-  }
+      slot.className = 'slot';
 
-  random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-}
+      let transform = `rotateX(${(slotAngle * i)}deg) translateZ(${this.REEL_RADIUS}px)`;
 
-const freshInsult = new Insult()
-const insult = freshInsult.generateInsult()
+      slot.style.transform = transform;
 
-const panelWidth = 120
-
-const SLOTS_PER_REEL = 16;
-const radius = Math.round( ( panelWidth / 2) / Math.tan( Math.PI / SLOTS_PER_REEL ) );
-// current settings give a value of 187, rounded to 190
-const REEL_RADIUS = radius;
-
-function createSlots(ring, options) {
-
-  var slotAngle = 360 / SLOTS_PER_REEL;
-
-  var seed = getSeed();
-
-  for (var i = 0; i < SLOTS_PER_REEL; i++) {
-    var slot = document.createElement('div');
-
-    slot.className = 'slot';
-
-    // compute and assign the transform for this slot
-    var transform = 'rotateX(' + (slotAngle * i) + 'deg) translateZ(' + REEL_RADIUS + 'px)';
-
-    slot.style.transform = transform;
-
-    // setup the number to show inside the slots
-    // the position is randomized to
-
-    var insultingWord = options[((seed + i) % 16)]
-    var content = $(slot).append(`<p>${insultingWord}</p>`);
-
-    // add the poster to the row
-    ring.append(slot);
-  }
-}
-
-function getSeed() {
-  // generate random number smaller than 16 then floor it to settle between 0 and 15 inclusive
-  return Math.floor(Math.random() * (SLOTS_PER_REEL));
-}
-
-function spin(timer) {
-  //var txt = 'seeds: ';
-  for (var i = 1; i < 4; i++) {
-    var oldSeed = -1;
-		/*
-		checking that the old seed from the previous iteration is not the same as the current iteration;
-		if this happens then the reel will not spin at all
-		*/
-    var oldClass = $('#ring' + i).attr('class');
-    if (oldClass.length > 4) {
-      oldSeed = parseInt(oldClass.slice(10));
-      console.log(oldSeed);
+      let insultingWord = options[((randomInt + i) % 16)]
+      let content = $(slot).append(`<p>${insultingWord}</p>`);
+      ring.append(slot);
     }
-    var seed = getSeed();
-    while (oldSeed == seed) {
-      seed = getSeed();
-    }
-
-    $('#ring' + i)
-      .css('animation', 'back-spin 1s, spin-' + seed + ' ' + (timer + i * 0.5) + 's')
-      .attr('class', 'ring spin-' + seed);
   }
 
-  console.log('=====');
+  getRandom() {
+    return Math.floor(Math.random() * (this.SLOTS_PER_REEL));
+  }
+
+  spin(timer) {
+    for (var i = 1; i < 4; i++) {
+      let oldRandomInt = -1;
+      /*
+      check if old random int from the previous iteration is not the same as the current iteration;
+      if this happens then the reel will not spin at all
+      */
+      let oldClass = $('#ring' + i).attr('class');
+      if (oldClass.length > 4) {
+        oldRandomInt = parseInt(oldClass.slice(10));
+      }
+      let randomInt = this.getRandom();
+      while (oldRandomInt == randomInt) {
+        randomInt = this.getRandom();
+      }
+
+      $('#ring' + i)
+        .css('animation', 'back-spin 1s, spin-' + randomInt + ' ' + (timer + i * 0.5) + 's')
+        .attr('class', 'ring spin-' + randomInt);
+    }
+  }
 }
+
 
 $(document).ready(function () {
+  const I = new Insult()
 
   // initiate slots
-  createSlots($('#ring1'), insults['adjective']);
-  createSlots($('#ring2'), insults['noun_one']);
-  createSlots($('#ring3'), insults['noun_two']);
+  I.createSlots($('#ring1'), I.adjectives);
+  I.createSlots($('#ring2'), I.firstNoun);
+  I.createSlots($('#ring3'), I.secondNoun);
 
   // hook start button
   $('.go').on('click', function () {
-    var timer = 2;
-    spin(timer);
+    let timer = 2;
+    I.spin(timer);
   })
 
   // hook xray checkbox
   $('#xray').on('click', function () {
-    //var isChecked = $('#xray:checked');
-    var tilt = 'tiltout';
+    let tilt = 'tiltout';
 
     if ($(this).is(':checked')) {
       tilt = 'tiltin';
@@ -140,9 +106,4 @@ $(document).ready(function () {
       }, 1900);
     }
   })
-
-  // hook perspective
-  // $('#perspective').on('click', function () {
-  //   $('#stage').toggleClass('perspective-on perspective-off');
-  // })
 });
